0e44efd0e2728c25e0a5499fc0bd6951226f1b37
khalid7487 <khalid15-7487@diu.edu.bd>
2019-11-13 20:32:20 +0600

    CURD

--- Hibernate_Intro/src/hibernate/Emp_Info.java
+++ Hibernate_Intro/src/hibernate/Emp_Info.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -15,7 +16,7 @@ import javax.persistence.Transient;
 public class Emp_Info {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int emp_id;
     @Column(name = "emoloyee_name", nullable = false)//user define colum name
     @Transient // jodi column database table e na chai
--- Hibernate_Intro/src/hibernate/Main.java
+++ Hibernate_Intro/src/hibernate/Main.java
@@ -14,16 +14,27 @@ import org.hibernate.SessionFactory;
  * @author KHALID
  */
 public class Main {
+
     public static void main(String[] args) {
-        Emp_Info emp=new Emp_Info();
-        emp.setEmp_id(2);
-        emp.setEmp_name("admin");
-        emp.setBirthdate(new Date());
-        try (SessionFactory sessionFactory = HibernateUtil.bulidsessionfactory(); Session session = sessionFactory.openSession())
-        {
-            session.beginTransaction();
-            session.save(emp);
-            session.getTransaction().commit();
-        }
+//        Emp_Info emp = new Emp_Info();
+//        //      emp.setEmp_id(2);
+//        emp.setEmp_name("admin");
+//        emp.setBirthdate(new Date());
+//        Emp_Info emp2 = new Emp_Info();
+//        emp2.setEmp_id(2);
+//        emp2.setEmp_name("admin");
+//        emp.setBirthdate(new Date());
+        SessionFactory sessionFact = HibernateUtil.getSessionFactory();
+        Session session = sessionFact.openSession();
+        session.beginTransaction();
+        
+        //this would save the Emp_info object into database
+//        session.save(emp);
+//        session.save(emp);
+        Emp_Info empinfo=(Emp_Info)session.get(Emp_Info.class, 1);
+        System.out.println(empinfo.getEmp_name());
+        session.getTransaction().commit();
+        session.close();
+        sessionFact.close();
     }
 }
